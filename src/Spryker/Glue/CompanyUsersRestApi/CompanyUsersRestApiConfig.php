02d6bc5bcecb947df62a8eb475f038d0617633c2
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CompanyUsersRestApi/CompanyUsersRestApiConfig.php
+++ src/Spryker/Glue/CompanyUsersRestApi/CompanyUsersRestApiConfig.php
@@ -12,21 +12,29 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class CompanyUsersRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_COMPANY_USERS = 'company-users';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_RESOURCE_COMPANY_USERS = 'company-users-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_COMPANY_USERS_GET_ACTION_NAME = 'get';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\CompanyBusinessUnitsRestApi\CompanyBusinessUnitsRestApiConfig::RESOURCE_COMPANY_BUSINESS_UNITS
      *
      * @var string
@@ -34,6 +42,8 @@ class CompanyUsersRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_COMPANY_BUSINESS_UNITS = 'company-business-units';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\CompanyRolesRestApi\CompanyRolesRestApiConfig::RESOURCE_COMPANY_ROLES
      *
      * @var string
@@ -41,31 +51,43 @@ class CompanyUsersRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_COMPANY_ROLES = 'company-roles';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_REST_USER_IS_NOT_A_COMPANY_USER = '1401';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_REST_USER_IS_NOT_A_COMPANY_USER = 'Rest user is not a company user.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_COMPANY_USER_NOT_SELECTED = '1403';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_COMPANY_USER_NOT_SELECTED = 'Current company user is not set. You need to select the current company user with /company-user-access-tokens in order to access the resource collection.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_COMPANY_USER_NOT_FOUND = '1404';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAIL_COMPANY_USER_NOT_FOUND = 'Company user not found';
@@ -85,6 +107,8 @@ class CompanyUsersRestApiConfig extends AbstractBundleConfig
     public const RESPONSE_DETAIL_COMPANY_USER_HAS_NO_PERMISSION = 'Forbidden';
 
     /**
+     * @api
+     *
      * @uses \Spryker\Glue\GlueApplication\GlueApplicationConfig::COLLECTION_IDENTIFIER_CURRENT_USER
      *
      * @var string
